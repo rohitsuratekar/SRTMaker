750802244e9ee13352a5454acebef3869a8de8ad
Rohit Suratekar <rohitsuratekar@gmail.com>
2016-12-08 13:55:46 +0530

    Corrected milliseconds to float conversion

--- Convert.py
+++ Convert.py
@@ -15,11 +15,13 @@ Start from first row - first column (A1) WITHOUT headers
 import pandas as pd
 
 column_names = ["text", "start", "start_milli", "end", "end_milli"]
-input_file = pd.ExcelFile("input.xlsx")
+input_file = pd.ExcelFile("input.xlsx", converters={'start_milli': str, 'end_milli': str})
 sheet1 = input_file.parse(0, header=None, names=column_names)
 counter = 1
 with open("output.srt", 'w') as file:
     for index, row in sheet1.iterrows():
+        milli1 = str(row["start_milli"]).split(".")[0].replace("nan", "000")
+        milli2 = str(row["end_milli"]).split(".")[0].replace("nan", "000")
         print("%d\n%s,%s --> %s,%s\n%s\n" % (
-            counter, row["start"], row["start_milli"], row["end"], row["end_milli"], row["text"]), file=file)
+            counter, row["start"], milli1, row["end"], milli2, row["text"]), file=file)
         counter += 1
