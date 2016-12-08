c13df32f37e2354697f5538d726b2edc4bd7544a
Rohit Suratekar <rohitsuratekar@gmail.com>
2016-12-08 13:32:24 +0530

    File input - output and format

--- Convert.py
+++ Convert.py
@@ -1,4 +1,25 @@
-import pandas as pd 
+"""
+SRTMaker
+Requirement : Python3 , Pandas Library
 
-xl = pd.ExcelFile("input.xlsx")
-print(xl.sheet_names)
+This script uses python3 to convert excel file into .srt file (used for subtitle in videos)
+excel file should be in following format
+Column 1: Text
+Column 2: start timing in (hh:mm:ss) format
+Column 3: milliseconds
+Column 4: end timing in (hh:mm:ss) format
+Column 5: milliseconds
+
+Start from first row - first column (A1) WITHOUT headers
+"""
+import pandas as pd
+
+column_names = ["text", "start", "start_milli", "end", "end_milli"]
+input_file = pd.ExcelFile("input.xlsx")
+sheet1 = input_file.parse(0, header=None, names=column_names)
+counter = 1
+with open("output.srt", 'w') as file:
+    for index, row in sheet1.iterrows():
+        print("%d\n%s,%s --> %s,%s\n%s\n" % (
+            counter, row["start"], row["start_milli"], row["end"], row["end_milli"], row["text"]), file=file)
+        counter += 1
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # SRTMaker
-Makes .SRT files from Excel or csv files
+Small python script which makes .SRT files from Excel or csv files
+
+Requirement : Python3 , Pandas Library
+
+This script uses python3 to convert excel file into .srt file (used for subtitle in videos)
+
+excel file should be in following format
+
+Column 1: Text <br>
+Column 2: start timing in (hh:mm:ss) format <br>
+Column 3: milliseconds <br>
+Column 4: end timing in (hh:mm:ss) format <br>
+Column 5: milliseconds <br>
+
+Start from first row - first column (A1) WITHOUT headers
+
+Just change file name to "input.xlsx" or change inside code :)
